71f05c9987ce85e0b09921b4e08da1e70cb98f1b
Anderson Reiner <reiner.academico@gmail.com>
2025-02-23 16:46:35 -0400

    inicializando projeto com spring initialz, com as depeências do spring web e lombok

---
+++ src/main/java/com/biblioteca/api_lib/ApiLibApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.api_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLibApplication.class, args);
+	}
+
+}
